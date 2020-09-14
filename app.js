254ea8d2a75f740f9a3a8c4d8d16f62eba71d25f
Tony Khaov <tony.khaov@gmail.com>
2020-09-14 18:40:37 +0200

    Remove Absol in the naming (= absolute) as it is obvious

--- app.js
+++ app.js
@@ -7,28 +7,27 @@ fs.readdir(DOWNLOADS_dir, {}, (err, files) => {
   if (err) return console.log(err);
 
   const artistFolders = files.filter((file) => file !== ".DS_Store");
-  const artistFoldersAbsolPaths = artistFolders.map((artistName) =>
+  const artistFoldersPaths = artistFolders.map((artistName) =>
     path.join(DOWNLOADS_dir, artistName)
   );
 
-  artistFoldersAbsolPaths.forEach((artistFolder) => {
+  artistFoldersPaths.forEach((artistFolder) => {
     fs.readdir(artistFolder, {}, (err, files) => {
       if (err) return console.log(err);
 
       const albumFolders = files.filter((file) =>
         /\(Album|Single\)$/.test(file)
       );
-      const albumFoldersAbsolPaths = albumFolders.map((albumName) =>
+      const albumFoldersPaths = albumFolders.map((albumName) =>
         path.join(artistFolder, albumName)
       );
 
-      albumFoldersAbsolPaths.forEach((albumFolder) => {
+      albumFoldersPaths.forEach((albumFolder) => {
         fs.readdir(albumFolder, {}, (err, files) => {
           if (err) console.log(err);
 
           const songs = files.filter((file) => /\.flac$/.test(file));
           const songsPaths = songs.map((song) => path.join(albumFolder, song));
-          console.log(songsPaths);
         });
       });
     });
