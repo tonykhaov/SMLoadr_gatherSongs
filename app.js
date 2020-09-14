67272e8f2a57d23451c4edd46cb7bac535c47e3c
Tony Khaov <tony.khaov@gmail.com>
2020-09-14 17:59:25 +0200

    Rename SMloadrDirectory to DOWNLOADS_dir and add fs.readdir

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const fs = require("fs");
 const path = require("path");
 
-const SMloadrDirectory = path.join(__dirname, "..");
+const DOWNLOADS_dir = path.join(__dirname, "/DOWNLOADS");
 
-console.log(SMloadrDirectory);
+console.log(DOWNLOADS_dir);
+
+fs.readdir(DOWNLOADS_dir, {}, (err, files) => {
+  if (err) return console.log(err);
+  else return console.log(typeof files);
+});
