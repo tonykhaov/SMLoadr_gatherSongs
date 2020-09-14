3a15041454053e29740599192d6aee3b3bd319fa
Tony Khaov <tony.khaov@gmail.com>
2020-09-14 18:08:42 +0200

    Get artistFoldersPaths based on artistFolders

--- app.js
+++ app.js
@@ -6,5 +6,8 @@ const DOWNLOADS_dir = path.join(__dirname, "/DOWNLOADS");
 fs.readdir(DOWNLOADS_dir, {}, (err, files) => {
   if (err) return console.log(err);
   const artistFolders = files.filter((file) => file !== ".DS_Store");
-  console.log(artistFolders);
+  const artistFoldersPaths = artistFolders.map((artistFolder) =>
+    path.join(DOWNLOADS_dir, artistFolder)
+  );
+  console.log(artistFoldersPaths);
 });
