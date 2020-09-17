29151f4311bd4663bca8f9f677b27fe807481b2a
Tony Khaov <tony.khaov@gmail.com>
2020-09-17 15:06:18 +0200

    Create moveDownloadedSongsToConvertedSongs function and call it inside IIFE

--- app.js
+++ app.js
@@ -6,41 +6,45 @@ const { getFiles } = require("./helpers.js");
 
 const DOWNLOADS_DIR = path.join(__dirname, "/DOWNLOADS");
 
-(async () => {
-  try {
-    const artists = await getFiles(DOWNLOADS_DIR);
+const moveDownloadedSongsToConvertedSongs = async () => {
+  const artists = await getFiles(DOWNLOADS_DIR);
 
-    const artistsPaths = artists
-      .filter((fileName) => fileName !== ".DS_Store")
-      .map((fileName) => path.join(DOWNLOADS_DIR, fileName));
+  const artistsPaths = artists
+    .filter((fileName) => fileName !== ".DS_Store")
+    .map((fileName) => path.join(DOWNLOADS_DIR, fileName));
 
-    artistsPaths.forEach(async (artistPath) => {
-      const albums = await getFiles(artistPath);
-      const albumsPaths = albums
-        .filter((file) => /\(Album|Single\)$/.test(file))
-        .map((album) => path.join(artistPath, album));
+  artistsPaths.forEach(async (artistPath) => {
+    const albums = await getFiles(artistPath);
+    const albumsPaths = albums
+      .filter((file) => /\(Album|Single\)$/.test(file))
+      .map((album) => path.join(artistPath, album));
 
-      albumsPaths.forEach((albumPath) => {
-        fs.readdir(albumPath, {}, (err, files) => {
-          const flacSongs = files.filter((file) => /\.flac$/.test(file));
+    albumsPaths.forEach((albumPath) => {
+      fs.readdir(albumPath, {}, (err, files) => {
+        const flacSongs = files.filter((file) => /\.flac$/.test(file));
 
-          flacSongs.forEach((flacSong) => {
-            const flacSongOldPath = path.join(albumPath, flacSong);
-            const flacSongsNewPath = path.join(
-              __dirname,
-              "ConvertedSongs",
-              flacSong
+        flacSongs.forEach((flacSong) => {
+          const flacSongOldPath = path.join(albumPath, flacSong);
+          const flacSongsNewPath = path.join(
+            __dirname,
+            "ConvertedSongs",
+            flacSong
+          );
+          fs.rename(flacSongOldPath, flacSongsNewPath, (err) => {
+            if (err) return console.log(err);
+            console.log(
+              `${flacSong} was succesfully moved to ConvertedSongs folder`
             );
-            fs.rename(flacSongOldPath, flacSongsNewPath, (err) => {
-              if (err) return console.log(err);
-              console.log(
-                `${flacSong} was succesfully moved to ConvertedSongs folder`
-              );
-            });
           });
         });
       });
     });
+  });
+};
+
+(() => {
+  try {
+    moveDownloadedSongsToConvertedSongs();
   } catch (e) {
     throw new Error(e);
   }
